ab76baae49bef95fad266b19ede6320d00963235
Joyoounjin <dbswls060@gmail.com>
2024-10-27 17:57:16 +0900

    JumpPad
    - 점프대 구현

---
+++ Sparta Dungeon/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 10f;  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
+
+
