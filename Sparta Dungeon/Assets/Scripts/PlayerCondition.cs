2bc893e96acc8be7169d9582ce4e8ce6f8208256
Joyoounjin <dbswls060@gmail.com>
2024-10-27 17:43:04 +0900

    Player Ui
    - 플레이어 체력, 스테미나 바 만들기

---
+++ Sparta Dungeon/Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition HP { get { return uiCondition.HP; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        HP.Subtract(HP.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (HP.curValue < 0.0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        HP.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어가 죽었다.");
+    }
+
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        HP.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
+}
